aeda28e741ca8353c6d48419edde3547bcd40690
CristianGaona <crgaonas24@gmail.com>
2020-04-17 01:20:02 -0500

    Agregando dependencia commons

--- src/main/java/com/formaci/nbdi/springboot/app/usuarios/CursoServicioUsuariosApplication.java
+++ src/main/java/com/formaci/nbdi/springboot/app/usuarios/CursoServicioUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.formaci.nbdi.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan({"com.formaci.nbdi.springboot.app.usuarios.commons.models.entity"})
 @SpringBootApplication
 public class CursoServicioUsuariosApplication {
 
--- src/main/java/com/formaci/nbdi/springboot/app/usuarios/ReposritoryConfig.java
+++ src/main/java/com/formaci/nbdi/springboot/app/usuarios/ReposritoryConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import com.formaci.nbdi.springboot.app.usuarios.models.entity.Rol;
-import com.formaci.nbdi.springboot.app.usuarios.models.entity.Usuario;
+import com.formaci.nbdi.springboot.app.usuarios.commons.models.entity.Rol;
+import com.formaci.nbdi.springboot.app.usuarios.commons.models.entity.Usuario;
 
 @Configuration
 public class ReposritoryConfig implements RepositoryRestConfigurer {
