4e9125e49f8b465a265b3f86ad34ac3392389a76
CristianGaona <crgaonas24@gmail.com>
2020-04-18 19:19:17 -0500

    Modificando datos de el import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('crisda', '12345', 1, 'Cristian', 'Gaona', 'crgaonas24@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('sebas', '12345', 1, 'Sebastian', 'Gaona', 'asgaonas25@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('crisda', '$2a$10$eyBcrCLJA.CvxTUnumCpfODGo.cg0InwFhT7aQN2g8Ed5MESuG0nq', 1, 'Cristian', 'Gaona', 'crgaonas24@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('sebas', '$2a$10$rCcI0QRsumRbqXAaxzhlTOBD1r3YaT/NDF7fc.Lqd/BXZCCoit.L.', 1, 'Sebastian', 'Gaona', 'asgaonas25@gmail.com');
 
 
-INSERT INTO `roles` (nombre) VALUES ('ROL_USER');
-INSERT INTO `roles` (nombre) VALUES ('ROL_ ADMIN');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
  
 INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (1, 2);
 INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (2, 2);
