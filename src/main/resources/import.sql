bdfdf53e1eed1bd3bf3ec939afdfa5ef40c1ebd0
Cristian Gaona <crgaonas24@gmail.com>
2020-05-01 17:04:41 -0500

    Configuración de Postgresql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('crisda', '$2a$10$eyBcrCLJA.CvxTUnumCpfODGo.cg0InwFhT7aQN2g8Ed5MESuG0nq', 1, 'Cristian', 'Gaona', 'crgaonas24@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('sebas', '$2a$10$rCcI0QRsumRbqXAaxzhlTOBD1r3YaT/NDF7fc.Lqd/BXZCCoit.L.', 1, 'Sebastian', 'Gaona', 'asgaonas25@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('crisda', '$2a$10$eyBcrCLJA.CvxTUnumCpfODGo.cg0InwFhT7aQN2g8Ed5MESuG0nq', true, 'Cristian', 'Gaona', 'crgaonas24@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('sebas', '$2a$10$rCcI0QRsumRbqXAaxzhlTOBD1r3YaT/NDF7fc.Lqd/BXZCCoit.L.', true, 'Sebastian', 'Gaona', 'asgaonas25@gmail.com');
 
 
-INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
-INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
  
-INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (1, 2);
-INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (2, 2);
-INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (2, 1);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (1, 2);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2, 2);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2, 1);
